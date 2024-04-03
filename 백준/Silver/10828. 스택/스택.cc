c273b1bfc8bf9b671b33fbeabd8b4098585ae513
Daeun <122000839+daeun084@users.noreply.github.com>
2024-04-03 13:13:16 +0900

    [Silver IV] Title: 스택, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.cc
+++ 백준/Silver/10828. 스택/스택.cc
@@ -1,40 +1,39 @@
 #include <iostream>
-#include <cstring>
+#include <stack>
+
 using namespace std;
 
 int main() {
-    int n, remain = -1, ip;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n, num;
+    string op;
+    stack<int> s;
     cin >> n;
-    int num[10001] = {0};
-    char ary[6];
+
 
     for (int i = 0; i < n; i++) {
-        cin >> ary;
+        cin >> op;
 
-        if (strcmp(ary, "push") == 0) {
-            cin >> ip;
-            remain++;
-            num[remain] = ip;
-        } else if (strcmp(ary, "pop") == 0) {
-            if (remain == -1)
-                cout << "-1\n";
+        if (op.compare("push") == 0) {
+            cin >> num;
+            s.push(num);
+        } else if (op.compare("pop") == 0) {
+            if (s.empty()) cout << "-1" << "\n";
             else {
-                cout << num[remain] << "\n";
-                num[remain] = 0;
-                remain--;
+                cout << s.top() << "\n";
+                s.pop();
             }
-        } else if (strcmp(ary, "size") == 0) {
-            cout << remain + 1 << "\n";
-        } else if (strcmp(ary, "empty") == 0) {
-            if (remain == -1)
-                cout << "1\n";
-            else
-                cout << "0\n";
-        } else if (strcmp(ary, "top") == 0) {
-            if (remain == -1)
-                cout << "-1\n";
-            else
-                cout << num[remain] << "\n";
+        } else if (op.compare("size") == 0) {
+            cout << s.size() << "\n";
+        } else if (op.compare("empty") == 0) {
+            if (s.empty()) cout << "1" << "\n";
+            else cout << "0" << "\n";
+        } else if (op.compare("top") == 0) {
+            if (s.empty()) cout << "-1" << "\n";
+            else cout << s.top() << "\n";
         }
     }
     return 0;
