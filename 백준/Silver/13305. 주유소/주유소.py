ceea90f16e71279867b11688f603d48d93bf5ea5
Daeun <122000839+daeun084@users.noreply.github.com>
2024-12-28 16:56:21 +0900

    [Silver III] Title: 주유소, Time: 120 ms, Memory: 48604 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -2,19 +2,16 @@ n = int(input())
 roads = list(map(int, input().split()))
 prices = list(map(int, input().split()))
 min_cost = 0
-min_idx = 0
+price_idx = 0
+roads_idx = 0
 
-# 1. 최소 값 vertex idx 구함
-for i in range(0, n - 1):
-    if prices[min_idx] > prices[i]:
-        min_idx = i
+while roads_idx < n - 1:
+    min_cost += prices[price_idx] * roads[roads_idx]
+    roads_idx += 1
 
-# 2. 처음 ~ 최소 vertex까지 움직임
-for i in range(0, min_idx):
-    min_cost += prices[0] * roads[i]
-
-# 3. 최소 vertex ~ 끝까지 움직임
-for i in range(min_idx, n - 1):
-    min_cost += prices[min_idx] * roads[i]
+    if prices[price_idx] > prices[price_idx + 1]:
+        price_idx += 1
+    elif prices[price_idx] > prices[roads_idx]:
+        price_idx = roads_idx
 
 print(min_cost)
