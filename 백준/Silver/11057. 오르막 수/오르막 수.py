5566977e22516c7fd0f096def17152a119206e86
Daeun <122000839+daeun084@users.noreply.github.com>
2025-02-20 20:55:05 +0900

    [Silver I] Title: 오르막 수, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/11057. 오르막 수/오르막 수.py
@@ -0,0 +1,9 @@
+n = int(input())
+dp = [[1 for _ in range(10)] for _ in range(n + 1)]
+
+for i in range(2, n + 1):
+    dp[i][0] = sum(dp[i - 1])
+    for j in range(1, 10):
+        dp[i][j] = dp[i][j - 1] - dp[i - 1][j - 1]
+
+print(sum(dp[n]) % 10007)
