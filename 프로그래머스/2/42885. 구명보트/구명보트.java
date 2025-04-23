4399006fa64fd391a5ae8f19d53cbd50734dd235
Daeun <122000839+daeun084@users.noreply.github.com>
2025-04-23 19:59:49 +0900

    [level 2] Title: 구명보트, Time: 12.97 ms, Memory: 55.6 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -3,24 +3,18 @@ import java.util.*;
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
-        
-        Arrays.sort(people);
         int small_idx = 0;
+        Arrays.sort(people);
         
         for (int i = people.length - 1; i >= 0; i--) {
-            if (people[i] == 0) {
+            if (small_idx > i) {
                 continue;
             }
-            
             if (people[small_idx] + people[i] <= limit) {
-                answer += 1;
-                people[small_idx] = 0;
-                people[i] = 0;
                 small_idx += 1;
-            } else {
-                answer += 1;
-                people[i] = 0;
             }
+            
+            answer += 1;            
         }
         return answer;
     }
