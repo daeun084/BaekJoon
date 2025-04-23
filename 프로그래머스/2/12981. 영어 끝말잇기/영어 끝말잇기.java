fa332ee2f9a1491d5ce379cb658e6ca2796aa256
Daeun <122000839+daeun084@users.noreply.github.com>
2025-04-23 20:33:43 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.18 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, String[] words) {
+        int cnt = 0;
+        
+        while (cnt < words.length) {
+            cnt += 1;
+            String word = words[cnt - 1];
+            
+            if (cnt > 1) {
+                String p_word = words[cnt - 2];
+
+                if (word.charAt(0) != p_word.charAt(p_word.length() -1)) {
+                    return handleAnswer(n, cnt);
+                }
+            }
+            
+            for (int i = 0; i < cnt - 1; i++) {
+                if (words[i].equals(word)) {
+                    return handleAnswer(n, cnt);
+                }
+            }
+        }
+        
+       return new int[]{0, 0};
+    }
+    
+    private int[] handleAnswer(int n, int cnt) {
+        if (cnt % n == 0) {
+            return new int[]{n, cnt / n};
+        } else {
+            return new int[]{cnt % n, cnt / n + 1};   
+        }
+    }
+}
+                                       
